d181527284693783c72d34c41bbffbe01cc2dfc1
cwelsh125 <65074305+cwelsh125@users.noreply.github.com>
2020-12-03 16:41:59 +0000

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,9 @@
 // app.js is the main routine to handle the PIDX product
-// code searches from the api.pidx.org:8080 website
+// code searches from the data api at the api.pidx.org:8080 website
 // Get a reference to the table body
 const tbody = d3.select("tbody") ;
 var tableData;
 var filteredData;
-var newdata;
-var returnData;
 
 var codeElement;
 var ProductdefElement;
@@ -28,20 +26,15 @@ var addResultString;
 
 var MxVal;
 var MnVal;
-var SearchFF;
 
 MnVal = 0;
 MxVal = 20;
-SearchFF = false;
 
-// Get all the product codes from the api.pidx.org API and load into data
-// top level collection is called 'result'
-
-d3.json('http://api.pidx.org:8080/api/v1/resources/codes/all', function(data) {
-  SearchFF = false ;
-  newdata = data;
-  tableData = newdata;
+// Get all the product codes from the api.pidx.org API and load into tableData variable
+// top level collection is called 'result', call displayData function to built the html table
+d3.json('http://api.pidx.org:8080/api/v1/resources/codes/all', function(tableData) {
   filteredData = tableData;
+  displaydata(tableData);
 
   // Select the input element and get the raw HTML node
   codeElement = d3.select("#code");
@@ -49,109 +42,68 @@ d3.json('http://api.pidx.org:8080/api/v1/resources/codes/all', function(data) {
   cetaneoctaneElement = d3.select("#cetane_octane");
   requesterElement = d3.select("#requester");
 
-  // Display the table data all rows from data initially
-  displaydata(tableData);
-
   // Set up the buttons
   button = d3.select("#filter-btn");
   nextButton = d3.select("#nextButton")
   prevButton = d3.select("#prevButton")
   resetButton= d3.select("#reset-btn")
 
-  //------ NEXT BUTTON ------
-  // Next button click function definition
+  //------ NEXT BUTTON ------ Next button click function definition
   nextButton.on("click", function(){
    if (MxVal < 3376){
     MnVal = MnVal + 20 ;
     MxVal = MxVal + 20 ;
-    if (SearchFF === false) {
-     displaydata(filteredData, minVal=MnVal, maxVal=MxVal);
-    } else if (SearchFF === true) {
-     displayfilterdata(filteredData, minVal=MnVal, maxVal=MxVal);
-    }
+    displaydata(filteredData, minVal=MnVal, maxVal=MxVal);
    }
   })
-  //------ END NEXT BUTTON ------
 
-  //------ PREV BUTTON ------
-  // Previous button click function definition
+  //------ PREV BUTTON ------ Previous button click function definition
   prevButton.on("click", function(){
    if (MnVal > 0){
     MnVal = MnVal - 20 ;
     MxVal = MxVal - 20 ;
-    if (SearchFF === false) {
-     displaydata(filteredData, minVal=MnVal, maxVal=MxVal);
-    }
-    if (SearchFF === true) {
-     displayfilterdata(filteredData, minVal=MnVal, maxVal=MxVal);
-    }
+    displaydata(filteredData, minVal=MnVal, maxVal=MxVal);
    }
   })
-  //------ END PREV BUTTON ------
 
-  //------ SEARCH BUTTON ------
-  // Search button "filter" click function definition, reset all data to original full table
+  //------ SEARCH BUTTON ------ Search button "filter" click function definition, reset all data to original full table
   button.on("click", function() {
-   SearchFF = true;
-   tableData = newdata;
    filteredData = tableData;
 
    // Get the value property of the input elements from the form and convert to upper case for filtering
-   codeValue = codeElement.property("value");
-   codeValue = codeValue.toUpperCase();
-   ProductdefValue = ProductdefElement.property("value");
-   ProductdefValue = ProductdefValue.toUpperCase();
-   cetaneoctaneValue = cetaneoctaneElement.property("value");
-   cetaneoctaneValue = cetaneoctaneValue.toString();
-   requesterValue = requesterElement.property("value");
-   requesterValue = requesterValue.toUpperCase();
+   codeValue = codeElement.property("value").toUpperCase();
+   ProductdefValue = ProductdefElement.property("value").toUpperCase();
+   cetaneoctaneValue = cetaneoctaneElement.property("value").toUpperCase();
+   requesterValue = requesterElement.property("value").toUpperCase();
 
-   // Set the codeValue filtered data and display to webpage
-   if (codeValue != "") filteredData = codeFilter(filteredData);
-
-   // Set the cetaneoctaneValue filtered data and display to webpage
-   if (cetaneoctaneValue != "") filteredData = cetaneoctaneFilter(filteredData);
-
-   // Set the requesterValue filtered data and display to webpage
-   if (requesterValue != "") filteredData = requesterFilter(filteredData);
-
-   // Set the ProductdefValue filtered data and display to webpage
-   if (ProductdefValue != "") filteredData = ProductdefFilter(filteredData);
-
-   // set min max and call displayFilteredData to redraw table
+   // Get filtered data based of inputs from form above, clear slic counters and display filtered data
    MnVal = 0 ;
    MxVal = 20 ;
-   displayfilterdata(filteredData, minVal=MnVal, maxVal=MxVal);
+   filteredData = combinedFilter(filteredData);
+   displaydata(filteredData, minVal=MnVal, maxVal=MxVal);
   })
-  //------ END SEARCH BUTTON ------
 
-
-  //------ RESET BUTTON ------
-  //  resetting the displayed data to the full dataset while keeping the filter values untouched
+  //------ RESET BUTTON ------ resetting the displayed data to the full dataset while keeping the filter values untouched
   resetButton.on("click", function() {
-   SearchFF = false ;
    console.log("Clearing Old Entries");
    document.getElementById('code').value = '';
    document.getElementById('product_definition').value = '';
    document.getElementById('cetane_octane').value = '';
    document.getElementById('requester').value = '';
+
+   // Reset filtered data to complete table data and display filtered data
    MnVal = 0 ;
    MxVal = 20 ;
-   tableData=newdata;
-   displaydata(tableData, minVal=MnVal, maxVal=MxVal);
    filteredData = tableData;
+   displaydata(filteredData, minVal=MnVal, maxVal=MxVal);
   })
-  //------ END RESET BUTTON ------
 })
 
-
-//------ FUNCTION DEFINITIONS ------
-// Function to create and/or reset the entire display table
-function displaydata(dispdata, minVal=MnVal, maxVal=MxVal){
+//------ DISPLAYDATA FUNCTION ------ Function to create and/or reset the entire display table or display the filtered data
+function displaydata(filteredData, minVal=MnVal, maxVal=MxVal){
    //clearing previous search filters and table data
-   SearchFF = false ;
    tbody.text("");
-   let selection = dispdata.result.slice(minVal,maxVal);
+   let selection = filteredData.result.slice(minVal,maxVal);
    selection.forEach((ProductData) => {
      var row = tbody.append("tr");
      Object.entries(ProductData).forEach(([key, value]) => {
@@ -161,63 +113,33 @@ function displaydata(dispdata, minVal=MnVal, maxVal=MxVal){
    })
 }
 
-// Function to create a filtered display table
-function displayfilterdata(fdata, minVal=MnVal, maxVal=MxVal){
-  // Set filtered data flag to true , clear table data and recreate using filtered data
-  SearchFF = true ;
-  tbody.text("");
-  let selection = fdata.slice(minVal,maxVal);
-   selection.forEach((ProductData) => {
-    var row = tbody.append("tr");
-    Object.entries(ProductData).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
-    })
-  })
-}
-
-// Function to filter on codeValue ---------------------------------------
-function codeFilter(codetableData){
- if(!(codetableData.hasOwnProperty('result'))){
-  console.log("CODE Adding result to requester search");
-  addResultString = "{\"result\": " + JSON.stringify(codetableData) + "}";
-  codetableData = JSON.parse(addResultString);
+//------ COMBINED FILTER ------ Function to filter on all four search items from the form
+function combinedFilter(filteredData){
+// if not result top level node, add one
+ if(!(filteredData.hasOwnProperty('result'))){
+  addResultString = "{\"result\": " + JSON.stringify(filteredData) + "}";
+  filteredData = JSON.parse(addResultString);
  }
- let codereturnData = codetableData['result'].filter(d=>d.code.match(codeValue));
- return codereturnData
-}
 
-// Function to filter on ProductdefValue ---------------------------------
-function ProductdefFilter(prodtableData){
- if(!(prodtableData.hasOwnProperty('result'))){
-  console.log("PROD Adding result to requester search");
-  addResultString = "{\"result\": " + JSON.stringify(prodtableData) + "}";
-  prodtableData = JSON.parse(addResultString);
- }
- let prodreturnData = prodtableData['result'].filter(d=>d.product_definition.match(ProductdefValue));
- return prodreturnData
-}
+// Filter on codeValue and add top level result node
+ filteredData = filteredData['result'].filter(d=>d.code.match(codeValue));
+ addResultString = "{\"result\": " + JSON.stringify(filteredData) + "}";
+ filteredData = JSON.parse(addResultString);
 
-// Function to filter on cetaneoctaneValue -------------------------------
-function cetaneoctaneFilter(cettableData){
- if(!(cettableData.hasOwnProperty('result'))){
-  console.log("CET Adding result to requester search");
-  addResultString = "{\"result\": " + JSON.stringify(cettableData) + "}";
-  cettableData = JSON.parse(addResultString);
- }
- console.log("CET value = ", cetaneoctaneValue);
- let cetreturnData = cettableData['result'].filter(d=>d.cetane_octane.match(cetaneoctaneValue));
- return cetreturnData
-}
+// Filter now on ProductdefValue and add top level result node
+ filteredData = filteredData['result'].filter(d=>d.product_definition.match(ProductdefValue));
+ addResultString = "{\"result\": " + JSON.stringify(filteredData) + "}";
+ filteredData = JSON.parse(addResultString);
 
-// Function to filter on requesterValue ----------------------------------
-function requesterFilter(reqtableData){
- if(!(reqtableData.hasOwnProperty('result'))){
-  console.log("REQ Adding result to requester search");
-  addResultString = "{\"result\": " + JSON.stringify(reqtableData) + "}";
-  reqtableData = JSON.parse(addResultString);
- }
- let reqreturnData = reqtableData['result'].filter(d=>d.requester.match(requesterValue));
- return reqreturnData
+// Filter now on cetaneoctaneValue and add top level result node
+ filteredData = filteredData['result'].filter(d=>d.cetane_octane.match(cetaneoctaneValue));
+ addResultString = "{\"result\": " + JSON.stringify(filteredData) + "}";
+ filteredData = JSON.parse(addResultString);
+
+// Filter lastly on requesterValue and add top level result node
+ filteredData = filteredData['result'].filter(d=>d.requester.match(requesterValue));
+ addResultString = "{\"result\": " + JSON.stringify(filteredData) + "}";
+ filteredData = JSON.parse(addResultString);
+
+ return filteredData
 }
-//------ END OF FUNCTIONS DEFINITION ------
